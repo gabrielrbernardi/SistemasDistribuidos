819285d2e05c92240b3446ece6cbf515c905eae0
gabrielrbernardi <gabrielrbernardi@gmail.com>
2022-08-05 15:17:03 -0300

    final updates

--- pySync01.py
+++ pySync01.py
@@ -2,7 +2,6 @@ from time import sleep
 import plyvel
 import pysyncobj
 from pysyncobj import SyncObjConf, replicated_sync, replicated
-from functools import partial
 
 ports = ["20000", "20001", "20002"]
 encoding = "UTF-8"
@@ -28,11 +27,11 @@ class MyCounter(pysyncobj.SyncObj):
             print("DB Aberto")
             return db
     
-    @replicated_sync  
+    @replicated_sync
     def insertKV(self, key, value):
         try:
             self.db.put(bytes(key, encoding), bytes(value, encoding))
-            print("Valor inserido ou atualizado com sucesso")
+            print("\nValor inserido ou atualizado com sucesso")
         except:
             print("Erro na insercao do valor")
 
@@ -48,45 +47,50 @@ class MyCounter(pysyncobj.SyncObj):
         return self.db.get(bytes(key, encoding))
 
     def initServer(self):
-        while True:
-            print("1 - Inserir chave:valor")
-            print("2 - Retornar valor, por chave")
-            print("3 - Excluir valor")
-            typeServer = int(input("Digite a operacao desejada (-1 para retornar): "))
-            if(typeServer == 1):
-                while True:
-                    key = input("Digite para continuar: ")
-                    if key == "-1":
-                        break
-                    value = input("Digite para continuar 2: ")
-                    instancia.insertKV(key, value)
-                    print(self.getValue(key))
-                    sleep(1)
-            elif(typeServer == 2):
-                while True:
-                    choose = str(input("Digite a chave para retornar o valor: "))
-                    if choose == "-1":
-                        break
-                    print(self.getValue(choose))
-            elif(typeServer == 3):
-                while True:
-                    choose = str(input("Digite a chave excluir o valor: "))
-                    if choose == "-1":
-                        break
-                    self.deleteKV(choose)
+        try:
+            while True:
+                if self._getLeader() == None:
+                    self.eleicao()
+                print("1 - Inserir chave:valor")
+                print("2 - Retornar valor, por chave")
+                print("3 - Excluir valor")
+                typeServer = int(input("Digite a operacao desejada (-1 para retornar): "))
+                if(typeServer == 1):
+                    while True:
+                        key = input("Digite para continuar: ")
+                        if key == "-1":
+                            break
+                        value = input("Digite para continuar 2: ")
+                        instancia.insertKV(key, value)
+                        print(self.getValue(key))
+                        sleep(1)
+                elif(typeServer == 2):
+                    while True:
+                        choose = str(input("Digite a chave para retornar o valor: "))
+                        if choose == "-1":
+                            break
+                        print(self.getValue(choose))
+                elif(typeServer == 3):
+                    while True:
+                        choose = str(input("Digite a chave excluir o valor: "))
+                        if choose == "-1":
+                            break
+                        self.deleteKV(choose)
+        except:
+            print("Erro")
         
-hosts = ["5000", "5001", "5002"]
+    def eleicao(self):
+        print("Eleicao")
+        while True:
+            if(instancia.isReady()) == True:
+                print("Lider: " + str(instancia._getLeader()))
+                break
+            else:
+                print(".")
+                sleep(1)
 
-def eleicao():
-    print("Eleicao")
-    while True:
-        if(instancia.isReady()) == True:
-            print(instancia._getLeader())
-            print("Pronto")
-            break
-        else:
-            print(".")
-            sleep(1)
+############# main #############
+hosts = ["5000", "5001", "5002"]
 
 def serverId():
     print(" 1 - localhost:5000")
@@ -102,5 +106,4 @@ def serverId():
 
 id = serverId()
 instancia = MyCounter(id[0], id[1])
-eleicao()
 instancia.initServer()
